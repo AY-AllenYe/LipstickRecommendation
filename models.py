1a8a6109c1e9d1665bc483fb064960c8a1c6b0f3
AY-AllenYe <919874668@qq.com>
2025-11-20 11:47:38 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
 import jittor as jt
 from jittor.models import resnet50, Resnext50_32x4d, resnet18, resnet101, vgg16_bn
-# from resnet import resnet50, Resnext50_32x4d, resnet18, resnet101
 
-def get_model(num_classes=6):
+def get_model(num_classes=5):
     model = resnet50(pretrained=True)
     # model = resnet18(pretrained=True)
     # model = Resnext50_32x4d(pretrained=True)
