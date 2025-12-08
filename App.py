e107d85f2c34a11956bda0b8f2e3638d2e0504a4
AY-AllenYe <919874668@qq.com>
2025-12-08 13:20:30 +0800

    Create App.py

---
+++ App.py
@@ -0,0 +1,36 @@
+# Input: 
+#   1.HEX(fetch by the color plate or RGB (adjusted by movable switches) and show the color real-time)
+#   2.Image(recognize the lip's mean color, Not Done Yet)
+#   3.Video_capture(Same as Image, Not Done Yet)
+# Need: 
+#   1.Btn*1(OpenImage...); SmallBtn*2(Left-Rotation?, Right-Rotation?)
+#   2.Btn*1(OpenCapture(On/Off)?)
+#   3.Btn*3(FetchColor(and show)?, AdjustColor..., Return?)
+#   4.MoveableBtn*3(AdjustColor - R,G,B)
+#   5.Widge*1(Image/Capture)
+#   6.PhotoDisplay*1(color)
+
+# Recommendation:
+#   Functions.
+# Need:
+#   1.Btn*2(SetDefault..., Recommendation?)
+#   1.Btn*1(Re-Inference?)
+#   2.TextLabel*1(Inference Logs)
+
+# Output: 
+#   1.List(similar lipsticks)
+#   2.Virtual Try-on (Video Capture covered by new-lips (click the item in List))
+# Need:
+#   1.ListTable*1(Recommemded List)
+#   2.Btn*1(DisplayInfo?)
+#   3.PhotoDisplay(Lipstick)
+#   4.TextLabel(DisplayInfo)
+#   2.Btn*1(Application on real-time capture(On/Off)?)
+#   3.Widge*1(VTon)
+
+# Others:
+#   1.Clear
+#   2.Quit
+# Need:
+#   1.Btn*1(Clear?)
+#   2.Btn*1(Quit?)
